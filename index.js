c8b2893344335bf0c490659e6df1a917e635648f
MarcoMondelli91 <96835853+MarcoMondelli91@users.noreply.github.com>
2022-02-15 11:37:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const drawContainer = (containerSize, childSize, numberOfChildren) => {};
 
 drawContainer(200, 50, 17);
+
+var squares = document.getElementsByClassName('box');
+
+
+
+
+for(var i = 0; i < squares.length; i++) {
+squares[i].addEventListener("click", changeColor);
+squares[i].addEventListener("mouseover", changeColor);
+squares[i].addEventListener("mouseover", deleteBox);
+}
+
+
+function changeColor(event) {
+  this.style.backgroundColor = randomColor();
+
+}
+
+function deleteBox(event){
+    setTimeout( () => { this.style.display = 'none'}, 2000)
+}
+
+function randomColor() {
+
+var randomRed = Math.floor(Math.random() * 255);
+var randomGreen = Math.floor(Math.random() * 255);
+var randomBlue = Math.floor(Math.random() * 255);
+//create the string that is the ‘random color’
+var randomColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
+
+return randomColor;
+}
